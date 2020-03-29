2a3b7e55a9fd56679f6b0cc0f7dff133d434448d
galbt <galbt91@gmail.com>
2020-03-30 00:52:59 +0300

    added login & signup pages

--- src/components/pages/Sign_up.js
+++ src/components/pages/Sign_up.js
@@ -5,35 +5,32 @@ class Sign_up extends Component{
     render() {
         return (
             <div className="container-fluid">
+                <h1 id="sign_up">Sign up</h1>
+
+
                 <form>
 
                         <label for="fname">Full name</label>
                         <br></br>
                         <input type="text" id="fname" name="fname>"/>
-                        <br></br><br></br>
+                        <br></br>
                         <label htmlFor="email">Email</label>
                         <br></br>
                         <input type="text" id="email" name="email>"/>
-                        <br></br><br></br>
+                        <br></br>
                         <label htmlFor="password">Password</label>
                         <br></br>
                         <input type="password" id="password" name="password>"/>
-                        <br></br><br></br>
+                        <br></br>
                         <label htmlFor="phone">Phone</label>
                         <br></br>
                         <input type="text" id="phone" name="phone>"/>
                         <br></br>
-
                         <input type="submit" id="submit" value="Sign up"/>
-                        <br></br>
-
-
-
-
-
-
+                        <br></br><br></br>
 
                 </form>
+
 
 
 
