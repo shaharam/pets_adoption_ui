a4efe59c31d1fea7c7c87e9af9ba82e96393608b
galbt <galbt91@gmail.com>
2020-03-31 13:10:33 +0300

    added about and fixed login sign up pages

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import "./about.css";
 
 class About extends Component {
     render() {
@@ -8,6 +9,15 @@ class About extends Component {
                 <h1>
                     About content
                 </h1>
+
+                Appdoption is a free ,online searchable database of animals who need homes across the whole world.
+                Any user who signed up can hand over their pet, provide basic information about it, and get contacted
+                by any one who wishes to adopt it.
+                <br></br><br></br>
+
+                Our main goal is to facilitate the process of adoption by providing better communication and information about the pet.
+                <br></br><br></br>
+                You are welcome to contact us <a href="Contact_us" target="_blank" id="here">here</a>.
 
             </div>
         );
